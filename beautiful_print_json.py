78b88332c22c8cfe51289c008b7b05bd07ba46d0
ChrisChou2018 <1050434689@qq.com>
2019-10-30 16:34:52 +0800

    update my tools

---
+++ beautiful_print_json.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import json
+import pprint
+import sys
+
+def beautiful_print(json_data):
+    python_obj = json.loads(json_data)
+    pprint.PrettyPrinter().pprint(python_obj)
+
+if __name__ == "__main__":
+    json_data = raw_input("json data>>")
+    beautiful_print(json_data)
---
+++ send_mail.py
@@ -0,0 +1,37 @@
+from smtplib import SMTP_SSL
+from email.mime.text import MIMEText
+from email.header import Header
+import getpass
+
+class SendMail(object):
+    def __init__(self, user_eamil, mail_title="test email", mail_content="test email"):
+        self.host_server = 'smtp.qq.com' 
+        self.pwd = ""
+        self.sender_email = '1050434689@qq.com'
+        self.receiver = user_eamil if user_eamil else self.sender_email
+        self.mail_content = mail_content
+        self.mail_title = mail_title
+
+    def send(self):
+        self.pwd = getpass.getpass("input your password>>")
+        raw_input("press anykey continue..")
+        smtp = SMTP_SSL(self.host_server)
+        smtp.set_debuglevel(1)
+        smtp.ehlo(self.host_server)
+        smtp.login(self.sender_email, self.pwd)
+        
+        msg = MIMEText(self.mail_content, "plain", 'utf-8')
+        msg["Subject"] = Header(self.mail_title, 'utf-8')
+        msg["From"] = self.sender_email
+        msg["To"] = self.receiver
+        smtp.sendmail(self.sender_email, self.receiver, msg.as_string())
+        smtp.quit()
+
+def main():
+    try:
+        title = raw_input("email title>>")
+        content = raw_input("email content>>")
+        s = SendMail('1050434689@qq.com', mail_title=title, mail_content=content)
+        s.send()
+    except KeyboardInterrupt:
+        exit()
