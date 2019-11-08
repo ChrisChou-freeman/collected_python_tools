4a0086039fb3014995957379ee2929310e0a3338
ChrisChou2018 <1050434689@qq.com>
2019-11-08 18:19:49 +0800

    update

--- file_send_to_email.py
+++ file_send_to_email.py
@@ -3,8 +3,9 @@ from smtplib import SMTP_SSL
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.header import Header
-import getpass
 import sys
+import json
+import logging
 
 
 if sys.version_info[0]==2:
@@ -13,20 +14,26 @@ else:
     input = input
 
 class SendMail(object):
-    def __init__(self, user_eamil, mail_title="test email", mail_content="test email", file_path=""):
+    def __init__(
+        self,
+        sender_mail,
+        to_eamil,
+        mail_title="test email",
+        mail_content="test email",
+        file_path="",
+        pass_word=""
+    ):
         self.host_server = 'smtp.qq.com' # your smtp email server
-        self.pwd = ""
-        self.sender_email = '1050434689@qq.com'
-        self.receiver = user_eamil if user_eamil else self.sender_email
+        self.pwd = pass_word
+        self.sender_email = sender_mail
+        self.receiver = to_eamil if to_eamil else self.sender_email
         self.mail_content = mail_content
         self.mail_title = mail_title
         self.file_path = file_path
 
     def send(self):
-        self.pwd = getpass.getpass("input your password>>")
-        input("press anykey continue..")
         smtp = SMTP_SSL(self.host_server)
-        smtp.set_debuglevel(1)
+        # smtp.set_debuglevel(1)
         smtp.ehlo(self.host_server)
         smtp.login(self.sender_email, self.pwd)
         
@@ -48,25 +55,47 @@ class SendMail(object):
                 msg.attach(content2)
         except Exception:
             print("file reading error!!")
-            exit()
+            sys.exit()
         print("file reading success...")
         smtp.sendmail(self.sender_email, self.receiver, msg.as_string())
         smtp.quit()
 
 def main():
+    '''
+    //sender_conf.json
+    {
+        "your_email": "email",
+        "passward": "",
+        "send_to": "email",
+        "title": "test",
+        "content": "test",
+        "file_path": "./1.txt"
+    }
+    '''
+    r = open("./sender_conf.json", "r")
+    data = r.read()
+    data = json.loads(data)
+    r.close()
     try:
-        title = input("email title>>")
-        content = input("email content>>")
-        file_path = input("input your file_path>>")
+        title = data["title"]
+        content = data["content"]
+        file_path = data["file_path"]
+        your_email = data["your_email"]
+        passward = data["passward"]
+        to_email = data["send_to"]
         s = SendMail(
-            '1050434689@qq.com',
+            your_email,
+            to_email,
             mail_title=title,
             mail_content=content,
-            file_path=file_path
+            file_path=file_path,
+            pass_word=passward
         )
         s.send()
+        print("mail sent success..")
     except KeyboardInterrupt:
-        exit()
+        sys.exit()
 
 if __name__ == "__main__":
     main()
+    
