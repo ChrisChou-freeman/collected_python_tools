de0e9d3c5f7bca4c1eab5d90d7fbaecaf5d7a1e7
ChrisChou2018 <1050434689@qq.com>
2019-11-11 18:19:23 +0800

    update

--- qr_code_maker.py
+++ qr_code_maker.py
@@ -5,12 +5,12 @@ def main(data):
     qr = qrcode.QRCode(
         version=2,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=10,border=10
+        box_size=10,
     )
     qr.add_data(data)
     qr.make(fit=True)
     img = qr.make_image()
-    img.save("qr.png")
+    img.save("./qr.png")
 
 if __name__ == "__main__":
-    main("http://www.cnblogs.com/sfnz/")
+    main("https://chrischou2018.github.io/")
